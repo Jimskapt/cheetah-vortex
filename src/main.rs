f486da7b4b3fb33d000556767f1ba5eb0eefa063
Jimskapt <rami.programmer@gmail.com>
2019-05-31 19:33:41 +0200

    Adding 2 parameters : items per row and generation limit.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,41 @@ extern crate rand;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+
     let file_path;
     if args.len() <= 1 {
         file_path = "list.txt";
     } else {
         file_path = args[1].as_str();
     }
-    println!("Cheetah Vortex 1.0.0");
+
+    let items_per_row: u32;
+    if args.len() <= 2 {
+        items_per_row = 2;
+    } else {
+        let asked: u32 = args[2].as_str().parse().unwrap();
+
+        if asked <= 0 {
+            items_per_row = 2;
+        } else {
+            items_per_row = asked;
+        }
+    }
+
+    let generation_limit: u32;
+    if args.len() <= 3 {
+        generation_limit = 10;
+    } else {
+        let asked: u32 = args[3].as_str().parse().unwrap();
+
+        if asked <= 0 {
+            generation_limit = 10;
+        } else {
+            generation_limit = asked;
+        }
+    }
+
+    println!("Cheetah Vortex 1.1.0");
     println!("Source file : {}\n", file_path);
 
     let mut raw_list = File::open(file_path)
@@ -39,9 +67,9 @@ fn main() {
         panic!("The file {} should not be empty", file_path);
     }
 
-    for _ in 0..10 {
+    for _ in 0..generation_limit {
         let mut name: String = "".to_string();
-        for _ in 0..2 {
+        for _ in 0..items_per_row {
             let id: f64 = rand::random();
             let id: usize = (id * list.len() as f64) as usize;
 
